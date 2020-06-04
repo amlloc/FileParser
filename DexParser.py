6649cfd61c460a0311c0c343d69f34001935f510
amlloc <ziweifire@gmail,com>
2020-06-05 00:23:15 +0800

    禁止套娃

--- DexParser.py
+++ DexParser.py
@@ -34,70 +34,76 @@ class Dexfile:
         f.seek(0x0, 0)
         self.dexHeader.magic = binascii.b2a_hex(f.read(8))
         f.seek(0x8, 0)
-        #binary data -> reverse -> Hexadecimal data -> Hexadecimal string  -> Hexadecimal
-        self.dexHeader.checksum = bytes.fromhex(binascii.b2a_hex(f.read(4)[::-1]).hex()).decode('utf-8')
+        #binary data -> reverse data -> Hexadecimal data -> Hexadecimal string  -> Hexadecimal
+        self.dexHeader.checksum = binascii.b2a_hex(f.read(4)[::-1]).decode('utf-8')
         f.seek(0xc, 0)
         self.dexHeader.signature = binascii.b2a_hex(f.read(20))
 
         f.seek(0x20, 0)
-        self.dexHeader.file_size = int(bytes.fromhex(binascii.b2a_hex(f.read(4)[::-1]).hex()).decode('utf-8'),16)
+        a = f.read(4)[::-1] # b'\x00u!\x90'
+        b = binascii.b2a_hex(a)  # b'00752190'
+        c = b.hex() #多余        '3030373532313930'
+        d = bytes.fromhex(c)  #多余  b'00752190'
+        e = d.decode('utf-8')  #'00752190'
+        # self.dexHeader.file_size = int(bytes.fromhex(binascii.b2a_hex(f.read(4)[::-1]).hex()).decode('utf-8'),16)
+        self.dexHeader.file_size = int(binascii.b2a_hex(f.read(4)[::-1]).decode('utf-8'),16)
 
         f.seek(0x24, 0)
-        self.dexHeader.header_size = int(bytes.fromhex(binascii.b2a_hex(f.read(4)[::-1]).hex()).decode('utf-8'),16)
+        self.dexHeader.header_size = int(binascii.b2a_hex(f.read(4)[::-1]).decode('utf-8'),16)
 
         f.seek(0x28, 0)
-        self.dexHeader.endian_tag = bytes.fromhex(binascii.b2a_hex(f.read(4)[::-1]).hex()).decode('utf-8')
+        self.dexHeader.endian_tag = binascii.b2a_hex(f.read(4)[::-1]).decode('utf-8')
 
         f.seek(0x2c, 0)
-        self.dexHeader.link_size = int(bytes.fromhex(binascii.b2a_hex(f.read(4)[::-1]).hex()).decode('utf-8'),16)
+        self.dexHeader.link_size = int(binascii.b2a_hex(f.read(4)[::-1]).decode('utf-8'),16)
 
         f.seek(0x30, 0)
-        self.dexHeader.link_off = int(bytes.fromhex(binascii.b2a_hex(f.read(4)[::-1]).hex()).decode('utf-8'),16)
+        self.dexHeader.link_off = int(binascii.b2a_hex(f.read(4)[::-1]).decode('utf-8'),16)
 
         f.seek(0x34, 0)
-        self.dexHeader.map_off =int(bytes.fromhex(binascii.b2a_hex(f.read(4)[::-1]).hex()).decode('utf-8'),16)
+        self.dexHeader.map_off =int(binascii.b2a_hex(f.read(4)[::-1]).decode('utf-8'),16)
 
         f.seek(0x38, 0)
-        self.dexHeader.string_ids_size = int(bytes.fromhex(binascii.b2a_hex(f.read(4)[::-1]).hex()).decode('utf-8'),16)
+        self.dexHeader.string_ids_size = int(binascii.b2a_hex(f.read(4)[::-1]).decode('utf-8'),16)
 
         f.seek(0x3c, 0)
-        self.dexHeader.string_ids_off = int(bytes.fromhex(binascii.b2a_hex(f.read(4)[::-1]).hex()).decode('utf-8'),16)
+        self.dexHeader.string_ids_off = int(binascii.b2a_hex(f.read(4)[::-1]).decode('utf-8'),16)
 
         f.seek(0x40, 0)
-        self.dexHeader.type_ids_size = int(bytes.fromhex(binascii.b2a_hex(f.read(4)[::-1]).hex()).decode('utf-8'),16)
+        self.dexHeader.type_ids_size = int(binascii.b2a_hex(f.read(4)[::-1]).decode('utf-8'),16)
 
         f.seek(0x44, 0)
-        self.dexHeader.type_ids_off = int(bytes.fromhex(binascii.b2a_hex(f.read(4)[::-1]).hex()).decode('utf-8'),16)
+        self.dexHeader.type_ids_off = int(binascii.b2a_hex(f.read(4)[::-1]).decode('utf-8'),16)
 
         f.seek(0x48, 0)
-        self.dexHeader.proto_ids_size = int(bytes.fromhex(binascii.b2a_hex(f.read(4)[::-1]).hex()).decode('utf-8'),16)
+        self.dexHeader.proto_ids_size = int(binascii.b2a_hex(f.read(4)[::-1]).decode('utf-8'),16)
         
         f.seek(0x4c, 0)
-        self.dexHeader.proto_ids_off = int(bytes.fromhex(binascii.b2a_hex(f.read(4)[::-1]).hex()).decode('utf-8'),16)
+        self.dexHeader.proto_ids_off = int(binascii.b2a_hex(f.read(4)[::-1]).decode('utf-8'),16)
 
         f.seek(0x50, 0)
-        self.dexHeader.field_ids_size = int(bytes.fromhex(binascii.b2a_hex(f.read(4)[::-1]).hex()).decode('utf-8'),16)
+        self.dexHeader.field_ids_size = int(binascii.b2a_hex(f.read(4)[::-1]).decode('utf-8'),16)
 
         f.seek(0x54, 0)
-        self.dexHeader.field_ids_off = int(bytes.fromhex(binascii.b2a_hex(f.read(4)[::-1]).hex()).decode('utf-8'),16)
+        self.dexHeader.field_ids_off = int(binascii.b2a_hex(f.read(4)[::-1]).decode('utf-8'),16)
 
         f.seek(0x58, 0)
-        self.dexHeader.method_ids_size = int(bytes.fromhex(binascii.b2a_hex(f.read(4)[::-1]).hex()).decode('utf-8'),16)
+        self.dexHeader.method_ids_size = int(binascii.b2a_hex(f.read(4)[::-1]).decode('utf-8'),16)
 
         f.seek(0x5c, 0)
-        self.dexHeader.method_ids_off = int(bytes.fromhex(binascii.b2a_hex(f.read(4)[::-1]).hex()).decode('utf-8'),16)
+        self.dexHeader.method_ids_off = int(binascii.b2a_hex(f.read(4)[::-1]).decode('utf-8'),16)
 
         f.seek(0x60, 0)
-        self.dexHeader.class_defs_size = int(bytes.fromhex(binascii.b2a_hex(f.read(4)[::-1]).hex()).decode('utf-8'),16)
+        self.dexHeader.class_defs_size = int(binascii.b2a_hex(f.read(4)[::-1]).decode('utf-8'),16)
 
         f.seek(0x64, 0)
-        self.dexHeader.class_defs_off = int(bytes.fromhex(binascii.b2a_hex(f.read(4)[::-1]).hex()).decode('utf-8'),16)
+        self.dexHeader.class_defs_off = int(binascii.b2a_hex(f.read(4)[::-1]).decode('utf-8'),16)
 
         f.seek(0x68, 0)
-        self.dexHeader.data_size = int(bytes.fromhex(binascii.b2a_hex(f.read(4)[::-1]).hex()).decode('utf-8'),16)
+        self.dexHeader.data_size = int(binascii.b2a_hex(f.read(4)[::-1]).decode('utf-8'),16)
 
         f.seek(0x6c, 0)
-        self.dexHeader.data_off = int(bytes.fromhex(binascii.b2a_hex(f.read(4)[::-1]).hex()).decode('utf-8'),16)
+        self.dexHeader.data_off = int(binascii.b2a_hex(f.read(4)[::-1]).decode('utf-8'),16)
 
     def print_header(self):
         print("[+] magic : {magic}".format(magic = self.dexHeader.magic))
@@ -127,14 +133,14 @@ class Dexfile:
     def init_map_list(self):
         map_off = int(self.dexHeader.map_off, 16)
         self.dexHeader.f.seek(map_off, 0)
-        map_size = int(bytes.fromhex(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).hex()).decode(), 16)
+        map_size = int(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).decode('utf-8'), 16)
         map_list = []
         for i in range(map_size):
             self.dexHeader.f.seek(map_off + i * 12 + 4, 0)
-            map_item_type = int(bytes.fromhex(binascii.b2a_hex(self.dexHeader.f.read(2)[::-1]).hex()).decode(), 16)
-            map_item_unused = int(bytes.fromhex(binascii.b2a_hex(self.dexHeader.f.read(2)[::-1]).hex()).decode(), 16)
-            map_item_size = int(bytes.fromhex(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).hex()).decode(), 16)
-            map_item_offset = int(bytes.fromhex(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).hex()).decode(), 16)
+            map_item_type = int(binascii.b2a_hex(self.dexHeader.f.read(2)[::-1]).decode('utf-8'), 16)
+            map_item_unused = int(binascii.b2a_hex(self.dexHeader.f.read(2)[::-1]).decode('utf-8'), 16)
+            map_item_size = int(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).decode('utf-8'), 16)
+            map_item_offset = int(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).decode('utf-8'), 16)
 
             map_list.append(DexMapItem(map_item_type, map_item_unused, map_item_size, map_item_offset))
         
@@ -146,14 +152,14 @@ class Dexfile:
 
         for i in range(string_ids_size):
             self.dexHeader.f.seek(string_ids_off + i * 4, 0)
-            string_data_off = int(bytes.fromhex(binascii.b2a_hex(self.dexHeader.f.read(1)[::-1]).hex()).decode(), 16)
+            string_data_off = int(binascii.b2a_hex(self.dexHeader.f.read(1)[::-1]).decode('utf-8'), 16)
             self.dexHeader.f.seek(string_data_off, 0)
 
             #pass the length byte
             self.dexHeader.f.read(1)
 
             length = 0
-            while int(bytes.fromhex(binascii.b2a_hex(self.dexHeader.f.read(1)[::-1]).hex()).decode(), 16) != 0:
+            while int(binascii.b2a_hex(self.dexHeader.f.read(1)[::-1]).decode('utf-8'), 16) != 0:
                 length += 1 
 
             self.dexHeader.f.seek(string_data_off + 1, 0)
@@ -179,7 +185,7 @@ class Dexfile:
 
         for i in range(type_ids_size):
             self.dexHeader.f.seek(type_ids_off + i * 4, 0)
-            descriptor_idx = int(bytes.fromhex(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).hex()).decode(), 16)
+            descriptor_idx = int(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).decode('utf-8'), 16)
             dex_type_id = DexTypeId(descriptor_idx)
             self.DexTypeIds.append(dex_type_id)
 
@@ -199,9 +205,9 @@ class Dexfile:
 
         for i in range(field_size):
             self.dexHeader.f.seek(field_off + i * 8, 0)
-            class_idx = int(bytes.fromhex(binascii.b2a_hex(self.dexHeader.f.read(2)[::-1]).hex()).decode(), 16)
-            type_idx = int(bytes.fromhex(binascii.b2a_hex(self.dexHeader.f.read(2)[::-1]).hex()).decode(), 16)
-            name_idx = int(bytes.fromhex(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).hex()).decode(), 16)
+            class_idx = int(binascii.b2a_hex(self.dexHeader.f.read(2)[::-1]).decode('utf-8'), 16)
+            type_idx = int(binascii.b2a_hex(self.dexHeader.f.read(2)[::-1]).decode('utf-8'), 16)
+            name_idx = int(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).decode('utf-8'), 16)
 
             field_id = DexFieldId(class_idx, type_idx, name_idx)
             self.DexFieldIds.append(field_id)
@@ -213,9 +219,9 @@ class Dexfile:
 
         for i in range(method_size):
             self.dexHeader.f.seek(method_off + i * 8, 0)
-            class_idx = int(bytes.fromhex(binascii.b2a_hex(self.dexHeader.f.read(2)[::-1]).hex()).decode(), 16)
-            proto_idx = int(bytes.fromhex(binascii.b2a_hex(self.dexHeader.f.read(2)[::-1]).hex()).decode(), 16)
-            name_idx = int(bytes.fromhex(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).hex()).decode(), 16)
+            class_idx = int(binascii.b2a_hex(self.dexHeader.f.read(2)[::-1]).decode('utf-8'), 16)
+            proto_idx = int(binascii.b2a_hex(self.dexHeader.f.read(2)[::-1]).decode('utf-8'), 16)
+            name_idx = int(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).decode('utf-8'), 16)
 
             method_id = DexFieldId(class_idx, proto_idx, name_idx)
             self.DexMethodIds.append(method_id)
@@ -226,9 +232,9 @@ class Dexfile:
 
         for i in range(proto_ids_size):
             self.dexHeader.f.seek(proto_ids_off + i * 16, 0)
-            shorty_idx =  int(bytes.fromhex(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).hex()).decode(), 16)
-            return_type_idx = int(bytes.fromhex(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).hex()).decode(), 16)
-            parameters_off = int(bytes.fromhex(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).hex()).decode(), 16)
+            shorty_idx =  int(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).decode('utf-8'), 16)
+            return_type_idx = int(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).decode('utf-8'), 16)
+            parameters_off = int(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).decode('utf-8'), 16)
 
             proto_id_item = DexProtoId(shorty_idx, return_type_idx, parameters_off)
             self.DexProtoIds.append(proto_id_item)
@@ -264,14 +270,14 @@ class Dexfile:
         for i in range(class_ids_size):
             
             self.dexHeader.f.seek(class_ids_off + i * 32, 0)
-            class_idx = int(bytes.fromhex(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).hex()).decode(), 16)
-            access_flags = int(bytes.fromhex(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).hex()).decode(), 16)
-            superclass_idx = int(bytes.fromhex(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).hex()).decode(), 16)
-            interfaces_off = int(bytes.fromhex(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).hex()).decode(), 16)
-            source_file_idx = int(bytes.fromhex(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).hex()).decode(), 16)
-            annotations_off = int(bytes.fromhex(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).hex()).decode(), 16)
-            class_data_off = int(bytes.fromhex(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).hex()).decode(), 16)
-            static_values_off = int(bytes.fromhex(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).hex()).decode(), 16)
+            class_idx = int(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).decode('utf-8'), 16)
+            access_flags = int(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).decode('utf-8'), 16)
+            superclass_idx = int(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).decode('utf-8'), 16)
+            interfaces_off = int(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).decode('utf-8'), 16)
+            source_file_idx = int(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).decode('utf-8'), 16)
+            annotations_off = int(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).decode('utf-8'), 16)
+            class_data_off = int(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).decode('utf-8'), 16)
+            static_values_off = int(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).decode('utf-8'), 16)
 
             dexClassDefObj = DexClassDef(class_idx, access_flags, superclass_idx, interfaces_off, source_file_idx, annotations_off, class_data_off,  static_values_off)
             self.DexClassDefs.append(dexClassDefObj)
@@ -304,7 +310,7 @@ class Dexfile:
                     
                 dexClassDataHeader.append(value)
             
-            staticFieldsSize = self.readUnsignedLeb128(dexClassDataHeader[0])
+            static_fields_size = self.readUnsignedLeb128(dexClassDataHeader[0])
             instance_fields_size = self.readUnsignedLeb128(dexClassDataHeader[1])
             direct_method_size  =  self.readUnsignedLeb128(dexClassDataHeader[2])
             virtual_method_size = self.readUnsignedLeb128(dexClassDataHeader[3])
@@ -328,7 +334,7 @@ class Dexfile:
             }
             """
 
-            for i in range(staticFieldsSize):
+            for i in range(static_fields_size):
                 array = []
                 length = 0
                 for j in range(2):
@@ -410,7 +416,7 @@ class Dexfile:
                 array.append(value)
                 method_idx = self.readUnsignedLeb128(array[0])
                 access_flags = self.readUnsignedLeb128(array[1])
-                codeOff = self.readUnsignedLeb128(array[2])
+                code_off = self.readUnsignedLeb128(array[2])
 
                 dexMethod = DexMethod(method_idx, access_flags, code_off)
                 dexMethod.offset = offset
@@ -439,19 +445,25 @@ class Dexfile:
                 array.append(value)
                 method_idx = self.readUnsignedLeb128(array[0])
                 access_flags = self.readUnsignedLeb128(array[1])
-                codeOff = self.readUnsignedLeb128(array[2])
+                code_off = self.readUnsignedLeb128(array[2])
 
                 dexMethod = DexMethod(method_idx, access_flags, code_off)
                 dexMethod.offset = offset
                 dexMethod.length = length
 
-                self.dexClassData.virtualMethods.append(dexMethod)
+                dexClassData.virtual_methods.append(dexMethod)
                 offset += length
             
-    def parseDexCode(self, codeOff):
-        self.dexHeader.f.seek(codeOff, 0)
-        registers_size = binascii.b2a_hex()
+    def parseDexCode(self, code_off):
+        self.dexHeader.f.seek(code_off, 0)
+        registers_size = int(binascii.b2a_hex(self.dexHeader.f.read(2)[::-1]).decode('utf-8'),16)
+        ins_size = int(binascii.b2a_hex(self.dexHeader.f.read(2)[::-1]).decode('utf-8'),16)
+        out_size = int(binascii.b2a_hex(self.dexHeader.f.read(2)[::-1]).decode('utf-8'),16)
+        tries_size = int(binascii.b2a_hex(self.dexHeader.f.read(2)[::-1]).decode('utf-8'),16)
+        debug_info_off = int(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).decode('utf-8'),16)
+        insns_size = int(binascii.b2a_hex(self.dexHeader.f.read(4)[::-1]).decode('utf-8'),16)
 
+        insns = bytes
 
 
     
